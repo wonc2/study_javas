a3037f413f8c53fe979ff40392e4d3e722795acb
wonc2 <wonclee112@gmail.com>
2024-08-04 00:21:58 +0900

    s

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-            Question question = new Question();
+            Quest question = new Quest();
             question.runQuest();
         }
 }
